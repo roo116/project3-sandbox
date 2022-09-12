1eac154a1facdacba452448d15ecf9d3c8265ee1
Chip Hill <hillkarl03@gmail.com>
2022-09-12 00:47:20 -0400

    initial commit

---
+++ server/models/index.js
@@ -0,0 +1,14 @@
+const Campsite = require('./Campsite');
+
+
+
+module.exports = {Campsite}
+
+
+
+
+// ORIGINAL CODE
+// const User = require('./User');
+// const Thought = require('./Thought');
+
+// module.exports = { User, Thought };
